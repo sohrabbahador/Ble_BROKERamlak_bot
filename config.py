9b3d494dcd8dbb55b356cb52c57064f06fb62726
sohrabbahador <sohrabbahador2@gmail.com>
2026-07-16 09:00:18 +0330

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ from pymongo import MongoClient
 TOKEN = "1163386061:P7CDH8D1hGtiZ1OB1-5jXuOClUgRK1y3TeU"
 BASE_URL = f"https://tapi.bale.ai/bot{TOKEN}"
 MAIN_CHANNEL_URL = "https://ble.ir/BROKER_amlak"
-ADMIN_ID = 123456789  # شناسه عددی سهراب بهادر (مدیر)
+ADMIN_ID = 160513400  # شناسه عددی سهراب بهادر (مدیر)
 
 # --- اتصال به دیتابیس ابری MongoDB ---
 MONGO_URI = "mongodb+srv://sohrabbahador2_db_user:48whO2iH0lCDGzeK@cluster0.tbsddnd.mongodb.net/?appName=Cluster0"
